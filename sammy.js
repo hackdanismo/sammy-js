e4f23edde5f578219c8b47f0dac2ab42ae9f025f
hackdanismo <office@justtoconfirm.co.uk>
2024-08-09 13:54:39 +0100

    feat: add property to output the version of the library as an optional output

--- sammy.js
+++ sammy.js
@@ -1,5 +1,8 @@
 (function(window) {
   const sammyLibrary = {
+    // Property that outputs the version of the library
+    version: "0.0.1",
+
     sammy(selector) {
       const methods = {
 
@@ -11,4 +14,6 @@
 
   // Keep the original namespace clean and only expose the methods in the library globally
   window.sammy = sammyLibrary.sammy;
+  // Expose the version number property globally
+  window.sammy.version = sammyLibrary.version;
 })(window);
